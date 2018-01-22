c82522e90e2636c9dce19188a668c027a61794dc
erinkahn <erin.kahn@atlanta.creativecircus.edu>
2018-01-22 18:41:16 -0500

    rope swing spritesheet and burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,23 @@
 (() => {
 	console.log("Hello World from app.js, transpiled and concatenated!");
 })();
+
+
+
+// when the user clicks the hamburger button
+document.querySelector(".burger").addEventListener("click", function() {
+	// it makes the nav visible and the hamburger change by adding a class to the body
+	document.body.classList.toggle("ul_open");
+	// show the menu items when you click
+
+	if (document.querySelector('.main-menu-items').style.display == 'block') {
+		document.querySelector('.main-menu-items').style.display = 'none';
+
+	} else {
+		document.querySelector('.main-menu-items').style.display = 'block';
+	}
+
+});
+	
+
+
