b7721c023c286dc9638e20d87cc2511f4ce9b54a
Erin Kahn <erinnicolekahn@gmail.com>
2018-08-13 15:37:36 -0400

    starting the js for create your own nerd

--- dist/js/createYourOwnNerd.js
+++ dist/js/createYourOwnNerd.js
@@ -4,108 +4,49 @@
 
 console.log('hello world');
 
-var bodyOptions = document.querySelector('.body ul');
+var bodyOptions = document.querySelectorAll('.body ul li');
 var bodyLeft = document.querySelector(".body-previous");
 var bodyRight = document.querySelector(".body-next");
 
-var eyesOptions = document.querySelector('.eyes ul');
+var eyesOptions = document.querySelectorAll('.eyes ul li');
 var eyesLeft = document.querySelector(".eyes-previous");
 var eyesRight = document.querySelector(".eyes-next");
 
-var mouthOptions = document.querySelector('.mouth ul');
+var mouthOptions = document.querySelectorAll('.mouth ul li');
 var mouthLeft = document.querySelector(".mouth-previous");
 var mouthRight = document.querySelector(".mouth-next");
 
-var shoesOptions = document.querySelector('.shoes ul');
+var shoesOptions = document.querySelectorAll('.shoes ul li');
 var shoesLeft = document.querySelector(".shoes-previous");
 var shoesRight = document.querySelector(".shoes-next");
 
-var bodyDeg = 0;
-var eyesDeg = 0;
-var mouthDeg = 0;
-var shoesDeg = 0;
+var bodyOption = 0;
+var eyesOption = 0;
+var mouthOption = 0;
+var shoesOption = 0;
 
 //body
+
+var changeBody = function changeBody() {
+	// hide all bodies
+	// show body numbered bodyOption
+
+};
+
 bodyLeft.onclick = function () {
-	bodyDeg = bodyDeg + 90;
-	bodyOptions.style.transform = 'rotateY(' + bodyDeg + 'deg)';
-	console.log("left");
+	bodyOption--;
+	if (bodyOption <= -1) {
+		bodyOption = bodyOptions.length - 1;
+	}
+	console.log("change to body", bodyOption);
+	changeBody();
 };
 bodyRight.onclick = function () {
-	bodyDeg = bodyDeg - 90;
-	bodyOptions.style.transform = 'rotateY(' + bodyDeg + 'deg)';
-	console.log("right");
+	bodyOption++;
+	if (bodyOption >= bodyOptions.length) {
+		bodyOption = 0;
+	}
+	console.log("change to body", bodyOption);
+	changeBody();
 };
-
-//eyes
-eyesLeft.onclick = function () {
-	eyesDeg = eyesDeg + 72;
-	eyesOptions.style.transform = 'rotateY(' + eyesDeg + 'deg)';
-	console.log("left");
-};
-eyesRight.onclick = function () {
-	eyesDeg = eyesDeg - 72;
-	eyesOptions.style.transform = 'rotateY(' + eyesDeg + 'deg)';
-	console.log("right");
-};
-
-//mouth
-mouthLeft.onclick = function () {
-	mouthDeg = mouthDeg + 45;
-	mouthOptions.style.transform = 'rotateY(' + mouthDeg + 'deg)';
-	console.log("left");
-};
-mouthRight.onclick = function () {
-	mouthDeg = mouthDeg - 45;
-	mouthOptions.style.transform = 'rotateY(' + mouthDeg + 'deg)';
-	console.log("right");
-};
-
-//shoes
-shoesLeft.onclick = function () {
-	shoesDeg = shoesDeg + 60;
-	shoesOptions.style.transform = 'rotateY(' + shoesDeg + 'deg)';
-	console.log("left");
-};
-shoesRight.onclick = function () {
-	shoesDeg = shoesDeg - 60;
-	shoesOptions.style.transform = 'rotateY(' + shoesDeg + 'deg)';
-	console.log("right");
-};
-
-// let currentFace = 0;
-// const faces = [
-// 	"../img/green-oreo.png",
-// 	"../img/blue-oreo.png",
-// 	"../img/pink-oreo.png",
-// 	"../img/red-oreo.png",
-// 	"../img/yellow-oreo.png",
-// ]
-
-// currentFace++;
-// change face image src to faces[currentFace] 
-
-// var deg = 0;
-
-// arrowsLeft.forEach(function(arrowLeft){
-// 	arrowLeft.onclick = function() {
-// 	deg = deg + 36;
-// 	images.forEach(function(image){
-// 		image.style.transform = `rotateY(${deg}deg)`;
-// 		console.log("left");
-// 		})
-// 	}
-// });
-
-// arrowsRight.forEach(function(arrowRight){
-// 	arrowRight.onclick = function() {
-// 		deg = deg - 36;
-// 	images.forEach(function(image){	
-// 		images.style.transform = `rotateY(${deg}deg)`;
-// 		console.log("right");
-// 	}
-// });
-
-///need to fix the images bc it's an array
-//# sourceMappingURL=makeMeAnOreo.js.map
 //# sourceMappingURL=createYourOwnNerd.js.map
--- src/js/createYourOwnNerd.js
+++ src/js/createYourOwnNerd.js
@@ -3,109 +3,48 @@
 // personalize your nerd
 console.log('hello world');
 
-let bodyOptions = document.querySelector('.body ul');
+let bodyOptions = document.querySelectorAll('.body ul li');
 let bodyLeft = document.querySelector(".body-previous");
 let bodyRight = document.querySelector(".body-next");
 
-let eyesOptions = document.querySelector('.eyes ul');
+let eyesOptions = document.querySelectorAll('.eyes ul li');
 let eyesLeft = document.querySelector(".eyes-previous");
 let eyesRight = document.querySelector(".eyes-next");
 
-let mouthOptions = document.querySelector('.mouth ul');
+let mouthOptions = document.querySelectorAll('.mouth ul li');
 let mouthLeft = document.querySelector(".mouth-previous");
 let mouthRight = document.querySelector(".mouth-next");
 
-let shoesOptions = document.querySelector('.shoes ul');
+let shoesOptions = document.querySelectorAll('.shoes ul li');
 let shoesLeft = document.querySelector(".shoes-previous");
 let shoesRight = document.querySelector(".shoes-next");
 
-let bodyDeg = 0;
-let eyesDeg = 0;
-let mouthDeg = 0;
-let shoesDeg = 0;
+let bodyOption = 0;
+let eyesOption = 0;
+let mouthOption = 0;
+let shoesOption = 0;
 
 //body
+
+let changeBody = function() {
+	// hide all bodies
+	// show body numbered bodyOption
+
+}
+
 bodyLeft.onclick = function () {
-	bodyDeg = bodyDeg + 90;
-	bodyOptions.style.transform = 'rotateY(' + bodyDeg + 'deg)';
-	console.log("left");
+	bodyOption--;
+	if (bodyOption <= -1) {
+		bodyOption = bodyOptions.length - 1;
+	}
+	console.log("change to body", bodyOption)
+	changeBody()
 };
 bodyRight.onclick = function () {
-	bodyDeg = bodyDeg - 90;
-	bodyOptions.style.transform = 'rotateY(' + bodyDeg + 'deg)';
-	console.log("right");
+	bodyOption++;
+	if (bodyOption >= bodyOptions.length) {
+		bodyOption = 0;
+	}
+	console.log("change to body", bodyOption)
+	changeBody()
 };
-
-//eyes
-eyesLeft.onclick = function () {
-	eyesDeg = eyesDeg + 72;
-	eyesOptions.style.transform = 'rotateY(' + eyesDeg + 'deg)';
-	console.log("left");
-};
-eyesRight.onclick = function () {
-	eyesDeg = eyesDeg - 72;
-	eyesOptions.style.transform = 'rotateY(' + eyesDeg + 'deg)';
-	console.log("right");
-};
-
-//mouth
-mouthLeft.onclick = function () {
-	mouthDeg = mouthDeg + 45;
-	mouthOptions.style.transform = 'rotateY(' + mouthDeg + 'deg)';
-	console.log("left");
-};
-mouthRight.onclick = function () {
-	mouthDeg = mouthDeg - 45;
-	mouthOptions.style.transform = 'rotateY(' + mouthDeg + 'deg)';
-	console.log("right");
-};
-
-//shoes
-shoesLeft.onclick = function () {
-	shoesDeg = shoesDeg + 60;
-	shoesOptions.style.transform = 'rotateY(' + shoesDeg + 'deg)';
-	console.log("left");
-};
-shoesRight.onclick = function () {
-	shoesDeg = shoesDeg - 60;
-	shoesOptions.style.transform = 'rotateY(' + shoesDeg + 'deg)';
-	console.log("right");
-};
-
-
-
-// let currentFace = 0;
-// const faces = [
-// 	"../img/green-oreo.png",
-// 	"../img/blue-oreo.png",
-// 	"../img/pink-oreo.png",
-// 	"../img/red-oreo.png",
-// 	"../img/yellow-oreo.png",
-// ]
-
-// currentFace++;
-// change face image src to faces[currentFace] 
-
-// var deg = 0;
-
-// arrowsLeft.forEach(function(arrowLeft){
-// 	arrowLeft.onclick = function() {
-// 	deg = deg + 36;
-// 	images.forEach(function(image){
-// 		image.style.transform = `rotateY(${deg}deg)`;
-// 		console.log("left");
-// 		})
-// 	}
-// });
-
-// arrowsRight.forEach(function(arrowRight){
-// 	arrowRight.onclick = function() {
-// 		deg = deg - 36;
-// 	images.forEach(function(image){	
-// 		images.style.transform = `rotateY(${deg}deg)`;
-// 		console.log("right");
-// 	}
-// });
-
-///need to fix the images bc it's an array
-//# sourceMappingURL=makeMeAnOreo.js.map
