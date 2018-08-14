e4216fc0015a3ff6b82567ff03728d38b54b107c
Erin Kahn <erinnicolekahn@gmail.com>
2018-08-14 11:14:45 -0400

    javascript works for create your own nerd
    had to loop through all the body element variables and add and remove a class that shows and hides them. had to link the left and right arrows to the length of each and go ++ or --

--- dist/js/createYourOwnNerd.js
+++ dist/js/createYourOwnNerd.js
@@ -16,21 +16,62 @@ var mouthOptions = document.querySelectorAll('.mouth ul li');
 var mouthLeft = document.querySelector(".mouth-previous");
 var mouthRight = document.querySelector(".mouth-next");
 
-var shoesOptions = document.querySelectorAll('.shoes ul li');
+var shoeOptions = document.querySelectorAll('.shoes ul li');
 var shoesLeft = document.querySelector(".shoes-previous");
 var shoesRight = document.querySelector(".shoes-next");
 
 var bodyOption = 0;
-var eyesOption = 0;
+var eyeOption = 0;
 var mouthOption = 0;
-var shoesOption = 0;
+var shoeOption = 0;
 
-//body
+//mouths------------------------------------------------
+var changeMouth = function changeMouth() {
+	// show mouth numbered mouthOption
+	for (var i = 0; i < mouthOptions.length; i++) {
+		console.log('mouthOptions[i]');
 
+		if (mouthOption == i) {
+			mouthOptions[i].classList.add('showThatMouth');
+		} else {
+			mouthOptions[i].classList.remove('showThatMouth');
+		}
+	}
+};
+
+mouthLeft.onclick = function () {
+	mouthOption--;
+	if (mouthOption <= -1) {
+		mouthOption = mouthOptions.length - 1;
+	}
+	console.log("change to mouth", mouthOption);
+	changeMouth();
+};
+
+mouthRight.onclick = function () {
+	mouthOption++;
+	if (mouthOption >= mouthOptions.length) {
+		mouthOption = 0;
+	}
+	console.log("change to mouth", mouthOption);
+	changeMouth();
+};
+
+changeMouth(); //need this to show the first eye by default
+
+
+//body------------------------------------------------
 var changeBody = function changeBody() {
-	// hide all bodies
-	bodyOptions.style.display = 'none';
 	// show body numbered bodyOption
+	for (var i = 0; i < bodyOptions.length; i++) {
+		console.log('bodyOptions[i]');
+
+		if (bodyOption == i) {
+			bodyOptions[i].classList.add('showThatBod');
+		} else {
+			bodyOptions[i].classList.remove('showThatBod');
+		}
+	}
 };
 
 bodyLeft.onclick = function () {
@@ -41,6 +82,7 @@ bodyLeft.onclick = function () {
 	console.log("change to body", bodyOption);
 	changeBody();
 };
+
 bodyRight.onclick = function () {
 	bodyOption++;
 	if (bodyOption >= bodyOptions.length) {
@@ -49,4 +91,76 @@ bodyRight.onclick = function () {
 	console.log("change to body", bodyOption);
 	changeBody();
 };
+
+changeBody(); //need this to show the first body by default
+
+
+//shoes------------------------------------------------
+var changeShoes = function changeShoes() {
+	// show shoe numbered shoeOption
+	for (var i = 0; i < shoeOptions.length; i++) {
+		console.log('shoeOptions[i]');
+
+		if (shoeOption == i) {
+			shoeOptions[i].classList.add('showThatShoe');
+		} else {
+			shoeOptions[i].classList.remove('showThatShoe');
+		}
+	}
+};
+
+shoesLeft.onclick = function () {
+	shoeOption--;
+	if (shoeOption <= -1) {
+		shoeOption = shoeOptions.length - 1;
+	}
+	console.log("change to shoe", shoeOption);
+	changeShoes();
+};
+
+shoesRight.onclick = function () {
+	shoeOption++;
+	if (shoeOption >= shoeOptions.length) {
+		shoeOption = 0;
+	}
+	console.log("change to shoe", shoeOption);
+	changeShoes();
+};
+
+changeShoes(); //need this to show the first shoe by default
+
+
+//eyes------------------------------------------------
+var changeEyes = function changeEyes() {
+	// show eye numbered eyeOption
+	for (var i = 0; i < eyesOptions.length; i++) {
+		console.log('eyesOptions[i]');
+
+		if (eyeOption == i) {
+			eyesOptions[i].classList.add('showThatEye');
+		} else {
+			eyesOptions[i].classList.remove('showThatEye');
+		}
+	}
+};
+
+eyesLeft.onclick = function () {
+	eyeOption--;
+	if (eyeOption <= -1) {
+		eyeOption = eyesOptions.length - 1;
+	}
+	console.log("change to eye", eyeOption);
+	changeEyes();
+};
+
+eyesRight.onclick = function () {
+	eyeOption++;
+	if (eyeOption >= eyesOptions.length) {
+		eyeOption = 0;
+	}
+	console.log("change to eye", eyeOption);
+	changeEyes();
+};
+
+changeEyes(); //need this to show the first eye by default
 //# sourceMappingURL=createYourOwnNerd.js.map
--- src/js/createYourOwnNerd.js
+++ src/js/createYourOwnNerd.js
@@ -15,22 +15,64 @@ let mouthOptions = document.querySelectorAll('.mouth ul li');
 let mouthLeft = document.querySelector(".mouth-previous");
 let mouthRight = document.querySelector(".mouth-next");
 
-let shoesOptions = document.querySelectorAll('.shoes ul li');
+let shoeOptions = document.querySelectorAll('.shoes ul li');
 let shoesLeft = document.querySelector(".shoes-previous");
 let shoesRight = document.querySelector(".shoes-next");
 
 let bodyOption = 0;
-let eyesOption = 0;
+let eyeOption = 0;
 let mouthOption = 0;
-let shoesOption = 0;
+let shoeOption = 0;
 
-//body
 
+//mouths------------------------------------------------
+let changeMouth = function() {
+	// show mouth numbered mouthOption
+	for (let i = 0; i < mouthOptions.length; i++) {
+		console.log('mouthOptions[i]');
+		
+		if (mouthOption == i){
+			mouthOptions[i].classList.add('showThatMouth');
+		} else {
+			mouthOptions[i].classList.remove('showThatMouth');
+		}
+	}
+}
+
+mouthLeft.onclick = function () {
+	mouthOption--;
+	if (mouthOption <= -1) {
+		mouthOption = mouthOptions.length - 1;
+	}
+	console.log("change to mouth", mouthOption);
+	changeMouth();
+};
+
+mouthRight.onclick = function () {
+	mouthOption++;
+	if (mouthOption >= mouthOptions.length) {
+		mouthOption = 0;
+	}
+	console.log("change to mouth", mouthOption);
+	changeMouth();
+};
+
+changeMouth(); //need this to show the first eye by default
+
+
+
+//body------------------------------------------------
 let changeBody = function() {
-	// hide all bodies
-	bodyOptions.style.display = 'none';
 	// show body numbered bodyOption
-
+	for (let i = 0; i < bodyOptions.length; i++) {
+		console.log('bodyOptions[i]');
+		
+		if (bodyOption == i){
+			bodyOptions[i].classList.add('showThatBod');
+		} else {
+			bodyOptions[i].classList.remove('showThatBod');
+		}
+	}
 }
 
 bodyLeft.onclick = function () {
@@ -38,14 +80,89 @@ bodyLeft.onclick = function () {
 	if (bodyOption <= -1) {
 		bodyOption = bodyOptions.length - 1;
 	}
-	console.log("change to body", bodyOption)
-	changeBody()
+	console.log("change to body", bodyOption);
+	changeBody();
 };
+
 bodyRight.onclick = function () {
 	bodyOption++;
 	if (bodyOption >= bodyOptions.length) {
 		bodyOption = 0;
 	}
-	console.log("change to body", bodyOption)
-	changeBody()
+	console.log("change to body", bodyOption);
+	changeBody();
 };
+
+changeBody(); //need this to show the first body by default
+
+
+
+//shoes------------------------------------------------
+let changeShoes = function() {
+	// show shoe numbered shoeOption
+	for (let i = 0; i < shoeOptions.length; i++) {
+		console.log('shoeOptions[i]');
+		
+		if (shoeOption == i){
+			shoeOptions[i].classList.add('showThatShoe');
+		} else {
+			shoeOptions[i].classList.remove('showThatShoe');
+		}
+	}
+}
+
+shoesLeft.onclick = function () {
+	shoeOption--;
+	if (shoeOption <= -1) {
+		shoeOption = shoeOptions.length - 1;
+	}
+	console.log("change to shoe", shoeOption);
+	changeShoes();
+};
+
+shoesRight.onclick = function () {
+	shoeOption++;
+	if (shoeOption >= shoeOptions.length) {
+		shoeOption = 0;
+	}
+	console.log("change to shoe", shoeOption);
+	changeShoes();
+};
+
+changeShoes(); //need this to show the first shoe by default
+
+
+
+//eyes------------------------------------------------
+let changeEyes = function() {
+	// show eye numbered eyeOption
+	for (let i = 0; i < eyesOptions.length; i++) {
+		console.log('eyesOptions[i]');
+		
+		if (eyeOption == i){
+			eyesOptions[i].classList.add('showThatEye');
+		} else {
+			eyesOptions[i].classList.remove('showThatEye');
+		}
+	}
+}
+
+eyesLeft.onclick = function () {
+	eyeOption--;
+	if (eyeOption <= -1) {
+		eyeOption = eyesOptions.length - 1;
+	}
+	console.log("change to eye", eyeOption);
+	changeEyes();
+};
+
+eyesRight.onclick = function () {
+	eyeOption++;
+	if (eyeOption >= eyesOptions.length) {
+		eyeOption = 0;
+	}
+	console.log("change to eye", eyeOption);
+	changeEyes();
+};
+
+changeEyes(); //need this to show the first eye by default
